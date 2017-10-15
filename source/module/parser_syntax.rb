9d0a9204d2e12aace95dc2ac2b18bfd775a70506
JSxNeet <JSxNeet@yandex.ru>
2017-10-16 01:53:29 +1100

    Fixed logical bug with empty strokes

--- source/module/parser_syntax.rb
+++ source/module/parser_syntax.rb
@@ -7,6 +7,7 @@ module Parser
     File.readlines(filename).each {
     
     |line| line = line.split(assignment_operator)
+    next if line.size == 1
     
     syntax_tree[line[0].strip] = line[1].to_i unless line[1].to_i == 0
     syntax_tree[line[0].strip] = line[1].strip if line[1].to_i == 0
