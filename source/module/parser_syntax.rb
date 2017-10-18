26be8051957e02431782e6ae3b3c74913b9af023
JSxNeet <JSxNeet@yandex.ru>
2017-10-18 16:00:48 +1100

    Updated to the third version

--- source/module/parser_syntax.rb
+++ source/module/parser_syntax.rb
@@ -1,18 +1,31 @@
-module Parser
-  def self.syntax( filename, assignment_operator, err_count )
-    return "-----\nError count not equally zero" if err_count > 0
+class Parser
+  def self.syntax( filename, assignment, array_operators, err_count )
+    return "-----\nError_count not equally zero" if err_count[:assigment] > 0 || err_count[:array] > 0
 
-    syntax_tree = Hash.new
-
+    parser_tree = Hash.new
+    
     File.readlines(filename).each {
+      
+    |line| next if line == "\n"
+    line = line.split(assignment)
+    arr = line[1].strip.split(array_operators).pop.strip.split(",")
     
-    |line| line = line.split(assignment_operator)
-    next if line.size == 1
-    
-    syntax_tree[line[0].strip] = line[1].to_i unless line[1].to_i == 0
-    syntax_tree[line[0].strip] = line[1].strip if line[1].to_i == 0
+    value_arr = arr.collect {
+    |element| if element.to_i == 0
+      element.strip
+    else
+      element.to_i
+    end
     }
+    
+    value = value_arr[0] unless value_arr.size != 1
+    
+    parser_tree[line[0].strip] = value_arr unless value_arr.size == 1
+    parser_tree[line[0].strip] = value.strip if value.to_i == 0 && !(value.nil?)
+    parser_tree[line[0].strip] = value.to_i unless value.to_i == 0
 
-    return syntax_tree
+    }
+  
+    return parser_tree
   end
 end
--- source/module/verification_syntax.rb
+++ source/module/verification_syntax.rb
@@ -1,18 +1,29 @@
-module Verification
-  def self.syntax ( filename, assignment_operator )
+class Verificator
+  def self.syntax (filename, assigment, array_operators)
     line_count = 0
-    err_count = 0
+    err_count = {:assigment => 0, :array => 0}
 
     File.readlines(filename).each {
-
-    |line| line = line.split(" ")
-
+    
+    |line| line_key__value = line.split(assigment)
+    next if line_key__value[0] == "\n"
     line_count += 1
 
-    next if line.empty?
-    err_count += 1 unless line.include?(assignment_operator)
-
-    puts "----------\nNo found assigment operator in #{filename} ---> stroke #{line_count}" unless line.include?(assignment_operator)
+    err_count[:assigment] += 1 if line_key__value.size == 1
+    puts "------\nUnderfined \"#{assigment}\" at line #{line_count} in #{filename}" if line_key__value.size == 1
+    next if line_key__value.size == 1
+    
+    err_count += 1 if line_key__value[1].nil?
+    arr = line_key__value[1].strip.split(" ")
+    arr_value = line_key__value[1].strip.split("|").pop.strip.split(",")
+    
+    array_count = 0
+    
+    unless arr_value.size == 1
+      arr.each {|word| array_count += 1 if word == array_operators}
+      err_count[:array] += 1 unless array_count == 2
+      puts "------\nUnderfined \"#{array_operators}\" at line #{line_count} in #{filename}" unless array_count == 2
+    end
     }
     
     return err_count
