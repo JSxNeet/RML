c057117b7e061c94ba35c64a63aab894daecf04f
JSxNeet <JSxNeet@yandex.ru>
2017-10-20 13:06:54 +1100

    removed the old source folder

--- source/example/main.rb
+++
@@ -1,7 +0,0 @@
-require_relative "../module/verification_syntax"
-require_relative "../module/parser_syntax"
-
-err_count = Verificator.syntax("example.rml", "=>", "|")
-parser_tree =  Parser.syntax("example.rml", "=>", "|", err_count)
-
-p parser_tree
